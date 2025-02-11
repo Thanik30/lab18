36fcf0219fae9b28629c5867237be6208d145383
Thanik30 <jeng.thanik@gmail.com>
2025-02-11 15:00:56 +0700

    lab18_1.cpp completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect X,Rect Y){
+	double x1 = max(X.x, Y.x);
+	double x2 = min(X.x+X.w, Y.x+Y.w);
+	double x = x2-x1;
+	double y1 = max(X.y-X.h, Y.y-Y.h);
+	double y2 = min(X.y, Y.y);
+	double y = y2-y1;
+	if(x*y>0) return x*y;
+	else return 0;
+}
+int main(){
+	Rect R1 = {1,1,5,5};
+    Rect R2 = {2,2,5,5};	
+	cout << overlap(R1,R2);	
+	return 0;
 }
